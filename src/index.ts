1c4dd091438e66fd91430a3212eefa8fb5933676
Dmitry Korzhov <korzhov.work2019@gmail.com>
2025-03-04 15:54:11 -0800

    remove dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
