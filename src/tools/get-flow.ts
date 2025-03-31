7718e7897b4908644f2a7e0ffd674bd954d53b7c
Dmitry Korzhov <korzhov.work2019@gmail.com>
2025-03-31 03:28:29 -0700

    flow endpoint

---
+++ src/tools/get-flow.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { BaseTool } from "../lib/base-tool";
+import { veyraxClient } from "../lib/client";
+
+const toolName = "get_flow";
+const toolDescription = `
+"Use this tool to retrieve a specific workflow by its ID.
+
+Workflow is sequence of steps that are executed in order to get some result. Flow comes with description, steps and input schema of all methods to call.
+
+You can call this tool once you have a flowId which usually you can get from: user directly OR using get-tools method."
+`;
+
+export class GetFlowTool extends BaseTool {
+  name = toolName;
+  description = toolDescription;
+
+  schema = z.object({
+    flowId: z.string().describe("The ID of the workflow to retrieve."),
+  });
+
+  async execute({ flowId }: z.infer<typeof this.schema>) {
+    try {
+      const { data } = await veyraxClient.get(`/get-flow/${flowId}`);
+
+      return {
+        content: [
+          {
+            type: "text" as const,
+            text: JSON.stringify(data, null, 2),
+          },
+        ],
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
--- src/tools/get-tools.ts
+++ src/tools/get-tools.ts
@@ -6,7 +6,9 @@ const toolName = "get_tools";
 const toolDescription = `
 "Use this tool to retrieve a list of available tools from the Veyrax API.
 This will return dynamic tools that user has access to.
-You can use this tool to get the list of tools, method names and parameters, and then use tool_call tool to call the tool with the provided parameters."
+You can use this tool to get the list of tools, method names and parameters, and then use tool_call tool to call the tool with the provided parameters.
+This method also returns all flows with name and id that user has access to (if any).
+"
 `;
 
 export class GetToolsTool extends BaseTool {
